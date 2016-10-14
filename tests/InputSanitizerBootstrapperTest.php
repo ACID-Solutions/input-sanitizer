7f01f1a430b191c64a1ea629642b4dfcb3b3717c
Daniel Lucas <daniel@acid.fr>
2016-10-14 18:17:16 +0200

    Added bootstrapper test

---
+++ tests/InputSanitizerBootstrapperTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Part of the InputSanitizer package.
+ *
+ * @package    InputSanitizer
+ * @version    1.0.0
+ * @author     Arthur Lorent <authur.lorent@gmail.com>, Daniel Lucas <daniel.chris.lucas@gmail.com>
+ * @license    MIT
+ * @copyright  (c) 2006-2016, Acid Solutions SARL
+ * @link       http://acid.fr
+ */
+
+namespace Acid\InputSanitizer\tests;
+
+use Acid\InputSanitizer\Native\InputSanitizerBootstrapper;
+use PHPUnit_Framework_TestCase;
+
+class InputSanitizerBootstrapperTest extends PHPUnit_Framework_TestCase
+{
+    public function testIntantiate()
+    {
+        $bootstrapper = new InputSanitizerBootstrapper();
+
+        $inputSanitizer = $bootstrapper->createInputSanitizer();
+
+        $this->assertInstanceOf(\Acid\InputSanitizer\InputSanitizer::class, $inputSanitizer);
+    }
+}
